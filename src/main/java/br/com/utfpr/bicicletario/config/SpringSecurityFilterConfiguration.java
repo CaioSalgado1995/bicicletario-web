9d1fac36380e92539e985c75aadc6948663acd10
Caio Luiz Salgado <clsalgado@scopus.com.br>
2019-01-08 20:22:57 -0200

    Removendo arquivos desnecessários.

--- src/main/java/br/com/utfpr/bicicletario/config/SpringSecurityFilterConfiguration.java
+++
@@ -1,7 +0,0 @@
-package br.com.utfpr.bicicletario.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer {
-
-}
